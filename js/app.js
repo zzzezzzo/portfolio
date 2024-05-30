023e0647068496eb9c2ec640fff4a56a565500b9
unknown <zeyadaboalsoud@gmail.com>
2024-05-30 16:00:41 +0300

    first commit the navbar

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const show = document.querySelector('.sidebar');
+    show.style.display = 'flex';
+}
+function headSidebar(){
+    const show = document.querySelector('.sidebar');
+    show.style.display = 'none';
+}
