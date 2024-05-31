9ea316f7bdcde8960b41672f2fdf167ed9bf2ef5
unknown <zeyadaboalsoud@gmail.com>
2024-06-01 02:29:06 +0300

    project commit first time

--- js/app.js
+++ js/app.js
@@ -6,3 +6,26 @@ function headSidebar(){
     const show = document.querySelector('.sidebar');
     show.style.display = 'none';
 }
+// this part is projects 
+let switchers = document.querySelectorAll('.switcher li');
+// let cards = Array.from(document.querySelectorAll('.project .card1'));
+let cards = document.querySelectorAll('.project .card1');
+
+switchers.forEach((li) =>{
+    li.addEventListener('click', mangementElement);
+    li.addEventListener('click', () =>{
+        switchers.forEach((li) =>{
+            li.classList.remove('active');
+        })
+        li.classList.add('active');
+    });
+})
+
+function mangementElement(){
+    cards.forEach((card) =>{
+        card.style.display = 'none';
+    })
+    document.querySelectorAll(this.dataset.category).forEach((el) => {
+        el.style.display = 'block';
+    })
+}
